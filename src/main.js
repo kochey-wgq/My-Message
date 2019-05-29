3ffbc6b41f2323a53fac85a58317ca2b62d29d2f
Your Name <you@example.com>
2019-05-29 22:03:12 +0800

    init file

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import styleInit from './assets/style/reset.css'
 
 Vue.config.productionTip = false
 
@@ -10,6 +11,7 @@ Vue.config.productionTip = false
 new Vue({
   el: '#app',
   router,
+  styleInit,
   components: { App },
   template: '<App/>'
 })
---
+++ src/pagees/home/components/Hander.vue
@@ -0,0 +1,13 @@
+<template>
+	<div>322</div>
+</template>
+
+<script>
+export default{
+	name : 'IndexHander'
+}
+</script>
+
+ <style>
+ 	
+ </style>
