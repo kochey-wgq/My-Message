bb59e0c04cbfc53617d2ef775528695f373c0137
Your Name <you@example.com>
2019-05-29 23:06:59 +0800

    init file

---
+++ src/pagees/home/Index.vue
@@ -0,0 +1,19 @@
+<template>
+	<div>
+		<index-hander></index-hander>
+	</div>
+</template>
+
+<script>
+import IndexHander from './components/Hander.vue'
+export default{
+	name : 'Index',
+	components : {
+		IndexHander
+	}
+}
+</script>
+
+ <style>
+ 	
+ </style>
